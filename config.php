25885e4f7e6f33fb8c2bca9d39cf1411014740b3
ychen188 <31746777+ychen188@users.noreply.github.com>
2017-12-03 16:00:55 -0500

    Update config.php

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 /* Database credentials */
 define('DB_SERVER', '66.147.242.186');
 define('DB_USERNAME', 'urcscon3_bronx');
-define('DB_PASSWORD', 'coffee');
+define('DB_PASSWORD', 'coffee1N');
 define('DB_NAME', 'urcscon3_bronx');
  
 
