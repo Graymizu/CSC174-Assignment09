1a2567c4730598aad859a159dbeaf530a2c12c6f
ychen188 <31746777+ychen188@users.noreply.github.com>
2017-12-03 22:17:47 -0500

    Update response-processor.php
    all the changes...

--- response-processor.php
+++ response-processor.php
@@ -13,8 +13,13 @@
 	*/	
 	
 	//Get values and escape strings
+	if (isset($_POST['fname'])) 
 	$fname = trim(stripslashes($_POST['fname']));
+
+	if (isset($_POST['lname'])) 
 	$lname = trim(stripslashes($_POST['lname']));
+
+	if (isset($_POST['email'])) 
 	$email = trim(stripslashes($_POST['email']));
 
 	//error fix for radio buttons and checkboxes
@@ -54,6 +59,7 @@
 	}else{
 		$siamese2f = $_POST['siamese2f']; 
 	}
+	if (isset($_POST['siamese3'])) 
 	$siamese3 = Trim(stripslashes($_POST['siamese3']));
 
 
@@ -93,6 +99,7 @@
 	}else{
 		$maine2f = $_POST['maine2f'];
 	}
+	if (isset($_POST['maine3'])) 
 	$maine3 = Trim(stripslashes($_POST['maine3']));
 
 	
@@ -131,6 +138,7 @@
 	}else{
 		$american2f = $_POST['american2f'];
 	}
+	if (isset($_POST['american3'])) 
 	$american3 = Trim(stripslashes($_POST['american3']));
 
 
@@ -169,6 +177,7 @@
 	}else{
 		$ragamuffin2f = $_POST['ragamuffin2f'];
 	}
+	if (isset($_POST['ragamuffin3'])) 
 	$ragamuffin3 = Trim(stripslashes($_POST['ragamuffin3']));
 
 	
@@ -207,62 +216,97 @@
 	}else{
 		$ragdoll2f = $_POST['ragdoll2f'];
 	}
+	if (isset($_POST['ragdoll3'])) 
 	$ragdoll3 = Trim(stripslashes($_POST['ragdoll3']));
 
-	$bengal1 = trim(stripslashes($_POST['bengal1']));
-	if(empty($_POST['bengal2a'])){
-		$bengal2a = $blank;
+
+	if(empty($_POST['bengal1'])){
+		$ragdoll1 = $blank;
 	}else{
-		$belgan2a = $_POST['bengal2a'];
+		$ragdoll1 = Trim(stripslashes($_POST['ragdoll1']));
 	}
-	if(empty($_POST['bengal2b'])){
-		$bengal2b = $blank;
+	if(empty($_POST['bengal2a'])){
+		$ragdoll2a = $blank;
 	}else{
-		$bengal2b = $_POST['bengal2b'];
+		$ragdoll2a = $_POST['bengal2a'];
+	}
+	if(empty($_POST['rbengal2b'])){
+		$ragdoll2b = $blank;
+	}else{
+		$ragdoll2b = $_POST['bengal2b'];
 	}
 	if(empty($_POST['bengal2c'])){
-		$bengal2c = $blank;
+		$ragdoll2c = $blank;
 	}else{
-		$bengal2c = $_POST['bengal2c'];
+		$ragdoll2c = $_POST['bengal2c'];
 	}
 	if(empty($_POST['bengal2d'])){
-		$bengal2d = $blank;
+		$ragdoll2d = $blank;
 	}else{
-		$bengal2d = $_POST['bengal2d'];
+		$ragdoll2d = $_POST['bengal2d'];
 	}
 	if(empty($_POST['bengal2e'])){
-		$bengal2e = $blank;
+		$ragdoll2e = $blank;
 	}else{
-		$bengal2e = $_POST['bengal2e'];
+		$ragdoll2e = $_POST['bengal2e'];
 	}
 	if(empty($_POST['bengal2f'])){
-		$bengal2f = $blank;
+		$ragdoll2f = $blank;
 	}else{
-		$bengal2f = $_POST['bengal2f'];
+		$ragdoll2f = $_POST['bengal2f'];
 	}
-	$bengal3 = Trim(stripslashes($_POST['bengal3']));
+	if (isset($_POST['bengal3'])) 
+	$ragdoll3 = Trim(stripslashes($_POST['bengal3']));
 	
-	$name = mysqli_real_escape_string($connection, $name);
+	if (isset($_POST['fname'])) { echo "fname";
+	$fname = mysqli_real_escape_string($connection, $fname);}
+	if (isset($_POST['lname'])) 
+	$lname = mysqli_real_escape_string($connection, $lname);
+	if (isset($_POST['email'])) 
 	$email = mysqli_real_escape_string($connection, $email);
+	if (isset($_POST['siamese1'])) 
 	$siamese1 = mysqli_real_escape_string($connection, $siamese1);
+	if (isset($_POST['maine1'])) 
 	$maine1 = mysqli_real_escape_string($connection, $maine1);
+	if (isset($_POST['american1'])) 
 	$american1 = mysqli_real_escape_string($connection, $american1);
+	if (isset($_POST['ragamuffin1'])) 
 	$ragamuffin1 = mysqli_real_escape_string($connection, $ragamuffin1);
+	if (isset($_POST['ragdoll1'])) 
 	$ragdoll1 = mysqli_real_escape_string($connection, $ragdoll1);
+	if (isset($_POST['bengal1'])) 
 	$bengal1 = mysqli_real_escape_string($connection, $bengal1);
+	if (isset($_POST['siamese3'])) 
 	$siamese3 = mysqli_real_escape_string($connection, $siamese3);
+	if (isset($_POST['maine3'])) 
 	$maine3 = mysqli_real_escape_string($connection, $maine3);
+	if (isset($_POST['american3'])) 
 	$american3 = mysqli_real_escape_string($connection, $american3);
+	if (isset($_POST['ragamuffin3'])) 
 	$ragamuffin3 = mysqli_real_escape_string($connection, $ragamuffin3);
+	if (isset($_POST['ragdoll3'])) 
 	$ragdoll3 = mysqli_real_escape_string($connection, $ragdoll3);
+	if (isset($_POST['bengal3'])) 
 	$bengal3 = mysqli_real_escape_string($connection, $bengal3);
 
 	// SQL Insert query into the database: responses
 
 	$query = "INSERT INTO response09 (";
-	$query .=" fname, lname, email, siamese1, siamese2, siamese3, maine1, maine2, maine3, american1, american2, american3";
+	$query .=" fname, lname, email, 
+		siamese1, siamese2a, siamese2b, siamese2c, siamese2d, siamese2e, siamese2f, siamese3, 
+		maine1, maine2a, maine2b, maine2c, maine2d, maine2e, maine2f, maine3, 
+		american1, american2a, american2b, american2c, american2d, american2e, american2f, american3, 
+		ragamuffin1, ragamuffin2a, ragamuffin2b, ragamuffin2c, ragamuffin2d, ragamuffin2e, ragamuffin2f, ragamuffin3, 
+		ragdoll1, ragdoll2a, ragdoll2b, ragdoll2c, ragdoll2d, ragdoll2e, ragdoll2f, ragdoll3, 
+		bengal1, bengal2a, bengal2b, bengal2c, bengal2d, bengal2e, bengal2f, bengal3";
 	$query .=") VALUES (";
-	$query .="'{$fname}', '{$lname}', '{$email}', '{$siamese1}', '{$siamese2}', '{$siamese3}', '{$maine1}', '{$maine2}', '{$maine3}', '{$american1}', '{$american2}', '{$american3}'";
+	$query .="'{$fname}','{$lname}', '{$email}', 
+	'{$siamese1}', '{$siamese2a}', '{$siamese2b}', '{$siamese2c}', '{$siamese2d}', '{$siamese2e}', '{$siamese2f}', '{$siamese3}', 
+	'{$maine1}', '{$maine2a}', '{$maine2b}', '{$maine2c}', '{$maine2d}', '{$maine2e}', '{$maine2f}', '{$maine3}', 
+	'{$american1}', '{$american2a}', '{$american2b}','{$american2c}','{$american2d}','{$american2e}','{$american2f}','{$american3}', 
+	'{$ragamuffin1}', '{$ragamuffin2a}', '{$ragamuffin2b}', '{$ragamuffin2c}', '{$ragamuffin2d}', '{$ragamuffin2e}', '{$ragamuffin2f}', '{$ragamuffin3}', 
+	'{$ragdoll1}', '{$ragdoll2a}', '{$ragdoll2b}', '{$ragdoll2c}', '{$ragdoll2d}', '{$ragdoll2e}', '{$ragdoll2f}', '{$ragdoll3}', 
+	'{$bengal1}', '{$bengal2a}', '{$bengal2b}', '{$bengal2c}', '{$bengal2d}', '{$bengal2e}', '{$bengal2f}', '{$bengal3}'";
 	$query .=")";
 	
 	$result = mysqli_query($connection, $query);
